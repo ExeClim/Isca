93990f40949ea8ff785709dfb3fa9c479a458d01
James Penn <james@jamespenn.co.uk>
2016-03-04 11:16:18 +0000

    Commented exp0

--- exp/jp492/exp0_reference_earth_grey.py
+++ exp/jp492/exp0_reference_earth_grey.py
@@ -7,28 +7,38 @@ import numpy as np
 
 import gfdl.experiment
 
-# Use one experiment to compile the source.  All other experiments
-# then use the same code with different namelist config
+# create an experiment based on the code version tagged `exoplan0.3`
 exp = gfdl.experiment.Experiment('ref_earth_grey',
     repo='git@github.com:jamesp/GFDLmoistModel.git',
     commit='exoplan0.3')
 
+# compiles source code to exp.execdir
 exp.compile()
 
+# setup the namelist:
+# - Frierson gray radiation
+# - No diurnal or seasonal cycle
+# - 25 vertical levels (26 half levels)
 exp.namelist['idealized_moist_phys_nml']['two_stream_gray'] = True
 exp.namelist['idealized_moist_phys_nml']['do_rrtm_radiation'] = False
 exp.namelist['two_stream_gray_rad_nml']['do_seasonal'] = False
 exp.namelist['spectral_dynamics_nml']['num_levels'] = 25
 
+# don't use a calendar, but do use 30 day "months"
 exp.namelist['main_nml'] = {
     'dt_atmos': 900,
     'seconds': 86400.0*30,
     'calendar': 'no_calendar'
 }
 
+# Setup a diag_table
+# - Record daily and every 6 hours
+# - Basic primitive equation prognostic var output
+# - Radiation fluxes
+
 diag = gfdl.experiment.DiagTable()
 
-#diag.add_file('6hourly', 6, 'hours')
+diag.add_file('6hourly', 6, 'hours')
 diag.add_file('daily', 1, 'days')
 
 diag.add_field('dynamics', 'ps')
@@ -46,14 +56,18 @@ diag.add_field('two_stream', 'flux_sw')
 diag.add_field('two_stream', 'flux_lw')
 diag.add_field('two_stream', 'tdt_rad')
 
-diag.add_field('mixed_layer', 't_surf')
-diag.add_field('mixed_layer', 'flux_oceanq')
+# diag.add_field('mixed_layer', 't_surf')
+# diag.add_field('mixed_layer', 'flux_oceanq')
 
-
-exp.clear_rundir()
-
+# add the diag_table setup to the experiment
 exp.use_diag_table(diag)
 
+# clean up previous runs.
+exp.clear_rundir()
+
+# run month 1 from a cold start
 exp.runmonth(1, use_restart=False)
 for i in range(2, 40):
+    # run subsequent months (default is to find the previous month
+    # and use that as restart).
     exp.runmonth(i)
