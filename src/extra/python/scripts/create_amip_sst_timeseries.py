883508eae8a700e26fe0cc33a7dd219a0ee3f2d1
sit23 <stephen.i.thomson@gmail.com>
2016-04-26 17:50:55 +0100

    Update create-amip-sst-timeseries to use updated create-timeseries.

--- src/extra/python/scripts/create_amip_sst_timeseries.py
+++ src/extra/python/scripts/create_amip_sst_timeseries.py
@@ -43,81 +43,37 @@ lonbs_adjusted[nlonb]=lonbs[-1,1]
 latbs_adjusted[0:nlatb]=latbs[:,0]
 latbs_adjusted[nlatb]=latbs[-1,1]
 
-
-
-
 day_number = resolution_file.variables['time'][:]
 
 time_arr = day_number_to_date(day_number, calendar_type = 'gregorian', units_in = 'days since 1870-1-1')
 
 time_arr_adj=np.arange(15,360,30)
 
+p_full=None
+p_half=None
 
-
-#DO FLIPPING
-
-
-
+npfull=None
+nphalf=None
 
 #Find grid and time numbers
 
 ntime=time_arr.day.shape[0]
 
 #Output it to a netcdf file. 
+file_name='sst_clim_amip_test.nc'
+variable_name='sst_clim_amip'
 
-sst_file = Dataset('sst_clim_amip.nc', 'w', format='NETCDF3_CLASSIC')
+number_dict={}
+number_dict['nlat']=nlat
+number_dict['nlon']=nlon
+number_dict['nlatb']=nlatb
+number_dict['nlonb']=nlonb
+number_dict['npfull']=npfull
+number_dict['nphalf']=nphalf
+number_dict['ntime']=ntime
 
-lat = sst_file.createDimension('lat', nlat)
-lon = sst_file.createDimension('lon', nlon)
+time_units='days since 0000-01-01 00:00:00.0'
 
-latb = sst_file.createDimension('latb', nlatb+1)
-lonb = sst_file.createDimension('lonb', nlonb+1)
+cts.output_to_file(sst_in,lats,lons,latbs_adjusted,lonbs_adjusted,p_full,p_half,time_arr_adj,time_units,file_name,variable_name,number_dict)
 
-time = sst_file.createDimension('time', 0) #s Key point for a year-independent climatology file is to have the length of the time axis 0, or 'unlimited'. This seems necessary to get the code to run properly. 
-
-latitudes = sst_file.createVariable('lat','d',('lat',))
-longitudes = sst_file.createVariable('lon','d',('lon',))
-
-latitudebs = sst_file.createVariable('latb','d',('latb',))
-longitudebs = sst_file.createVariable('lonb','d',('lonb',))
-
-times = sst_file.createVariable('time','d',('time',))
-
-latitudes.units = 'degrees_N'.encode('utf-8')
-latitudes.cartesian_axis = 'Y'
-latitudes.edges = 'latb'
-latitudes.long_name = 'latitude'
-
-longitudes.units = 'degrees_E'.encode('utf-8')
-longitudes.cartesian_axis = 'X'
-longitudes.edges = 'lonb'
-longitudes.long_name = 'longitude'
-
-latitudebs.units = 'degrees_N'.encode('utf-8')
-latitudebs.cartesian_axis = 'Y'
-latitudebs.long_name = 'latitude edges'
-
-longitudebs.units = 'degrees_E'.encode('utf-8')
-longitudebs.cartesian_axis = 'X'
-longitudebs.long_name = 'longitude edges'
-
-
-times.units = 'days since 0000-01-01 00:00:00.0'
-times.calendar = 'THIRTY_DAY_MONTHS'
-times.calendar_type = 'THIRTY_DAY_MONTHS'
-times.cartesian_axis = 'T'
-
-sst_array_netcdf = sst_file.createVariable('sst_clim_amip','f4',('time', 'lat','lon',))
-
-latitudes[:] = lats
-longitudes[:] = lons
-
-latitudebs[:] = latbs_adjusted
-longitudebs[:] = lonbs_adjusted
-
-times[:]     = time_arr_adj #date2num(time_arr,units='days since 0001-01-01 00:00:00.0',calendar='360_day')
-
-sst_array_netcdf[:] = sst_in[:,:,:]
-
-sst_file.close()
 
