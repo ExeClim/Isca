72e185625e910490a4bbb2b5601c155c2e8d3562
Ruth Geen <rg419@emps-gv2.ex.ac.uk>
2016-05-24 15:12:03 +0100

    Include very simple CO2 parametrisation in Byrne radiation scheme

--- src/atmos_param/two_stream_gray_rad/two_stream_gray_rad.F90
+++ src/atmos_param/two_stream_gray_rad/two_stream_gray_rad.F90
@@ -422,11 +422,11 @@ case(B_GEEN)
   ! split LW in 2 bands: water-vapour window and remaining = non-window
   ! ref: Ruth Geen etal, GRL 2016 (supp. information).
   do k = 1, n
-    lw_del_tau    = ( ir_tau_co2 + 0.2023 * log(carbon_conc/360)                  &
+    lw_del_tau    = ( ir_tau_co2 + 0.2023 * log(carbon_conc/360.)                  &
                     + ir_tau_wv*sqrt(q(:,:,k)) )                               &
                * ( p_half(:,:,k+1)-p_half(:,:,k) ) / p_half(:,:,n+1)
     lw_dtrans(:,:,k) = exp( - lw_del_tau )
-    lw_del_tau_win   = ( ir_tau_co2_win + 0.0954 * log(carbon_conc/360)           &
+    lw_del_tau_win   = ( ir_tau_co2_win + 0.0954 * log(carbon_conc/360.)           &
                                      + ir_tau_wv_win1*q(:,:,k)                 &
                                      + ir_tau_wv_win2*q(:,:,k)*q(:,:,k) )      &
                   * ( p_half(:,:,k+1)-p_half(:,:,k) ) / p_half(:,:,n+1)
@@ -453,7 +453,7 @@ case(B_BYRNE)
   !      Convective quasi-equilibrium theory and idealized simulations.
   !      J. Climate 26, 4000–4106 (2013).
   do k = 1, n
-    lw_del_tau    = (bog_a*bog_mu + bog_b*q(:,:,k)) * (( p_half(:,:,k+1)-p_half(:,:,k) ) / p_half(:,:,n+1))
+    lw_del_tau    = (bog_a*bog_mu + 0.17 * log(carbon_conc/360.)  + bog_b*q(:,:,k)) * (( p_half(:,:,k+1)-p_half(:,:,k) ) / p_half(:,:,n+1))
     lw_dtrans(:,:,k) = exp( - lw_del_tau )
 
   end do
