dad6a1a8afcd1d1e90db639e75ec3c9fc7944615
James Penn <james@jamespenn.co.uk>
2016-07-06 14:28:20 +0100

    Deleted new experiments as this directory has been moved into its own repo

--- exp/jp492/playground.py
+++ exp/jp492/playground.py
@@ -28,19 +28,9 @@ diag.add_field('two_stream', 'lw_dtrans')
 
 diag.add_field('mixed_layer', 't_surf')
 
-diag.add_field('dry_convection', 'dp')
-diag.add_field('dry_convection', 'CAPE')
-diag.add_field('dry_convection', 'CIN')
-diag.add_field('dry_convection',  'LZB')
-diag.add_field('dry_convection', 'LCL')
-diag.add_field('dry_convection', 'dt_tg')
-diag.add_field('dry_convection', 'parcel_temp')
-
-
 
 exp.use_diag_table(diag)
 
-#exp.clear_workdir()
 exp.disable_rrtm()
 exp.compile()
 
@@ -48,41 +38,14 @@ exp.clear_rundir()
 
 exp.namelist['main_nml'] = {
     'dt_atmos': 900,
-#    'seconds': 5*86400,
-    'days': 5,
-    'calendar': 'no_calendar',
-#    'current_date': [0001,1,1,0,0,0]
+    'seconds': 86400.0*10,
+    'calendar': 'no_calendar'
 }
 
-exp.namelist['two_stream_gray_rad_nml']['do_seasonal'] = True
+exp.namelist['two_stream_gray_rad_nml']['do_seasonal'] = False
 exp.namelist['spectral_dynamics_nml']['num_levels'] = 25
-exp.namelist['idealized_moist_phys_nml']['convection_scheme'] = 'dry'
-# exp.namelist['idealized_moist_phys_nml']['lwet_convection'] = False
-# exp.namelist['idealized_moist_phys_nml']['do_bm'] = False
 
-exp.namelist['dry_convection_nml'] = {
-    'tau': 86400.0*10,
-    'gamma': 1.0, # K/km
-}
 
-exp.namelist['lscale_cond_nml'] = {
-    'do_simple': True,
-    'do_evap': False,
-    'hc': 1.0,
-}
-
-exp.namelist['mixed_layer_nml'] = {
-    'albedo_value': 0.27,
-    'depth': 10.0,
-    #'prescribe_initial_dist': True
-    # 'tconst': 285.0,
-    # 'delta_T': 40.0,
-    'evaporation': False,
-    'do_qflux': False
-}
-
-exp.runmonth(1, use_restart=False)
-
-for i in range(10):
-    # exp.namelist['two_stream_gray_rad_nml']['rad_scheme'] = scheme
-    exp.runmonth(i+2)
+for i, scheme in enumerate(('frierson', 'geen', 'byrne')):
+    exp.namelist['two_stream_gray_rad_nml']['rad_scheme'] = scheme
+    exp.runmonth(i, use_restart=False)
