56ffa0a088b9d1c49690ade4df0b4d2465b341e3
sit23 <stephen.i.thomson@gmail.com>
2017-12-11 11:17:32 +0000

    Added compile script for plev.x to make using interpolation easier on other machines.

---
+++ postprocessing/plevel_interpolation/compile_plev_interpolation.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#Script that compiles the plev.x executable
+
+cd ./exec
+
+source $GFDL_BASE/src/extra/env/$GFDL_ENV
+
+../bin/mkmf -p plev.x -t ../bin/mkmf.template.ia64 -c "-Duse_netCDF" -a ../src ../src/path_names ../src/shared/mpp/include ../src/shared/include
+
+make -f Makefile
