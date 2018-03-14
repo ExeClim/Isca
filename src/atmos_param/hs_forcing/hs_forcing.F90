59ba1e484ac3f1de76da9f651c4a85f3e8c46ec5
James Penn <james@jamespenn.co.uk>
2018-03-14 16:00:52 +0000

    Revert the change to EXOPLANET forcing in hs_forcing.F90 for the docker changes

--- src/atmos_param/hs_forcing/hs_forcing.F90
+++ src/atmos_param/hs_forcing/hs_forcing.F90
@@ -584,21 +584,12 @@ real, intent(in),  dimension(:,:,:), optional :: mask
 
 !  ----- compute damping -----
       sigma(:,:) = p_full(:,:,k)*rps(:,:)
-      if(uppercase(trim(equilibrium_t_option)) == 'EXOPLANET') then
-        where (sigma(:,:) <= 1.0 .and. sigma(:,:) > sigma_b)
-          tfactr(:,:) = tcoeff*(sigma(:,:)-sigma_b)
-          tdamp(:,:,k) = tka + tfactr(:,:)
-        elsewhere
-          tdamp(:,:,k) = tka
-        endwhere
-      else
-        where (sigma(:,:) <= 1.0 .and. sigma(:,:) > sigma_b)
-          tfactr(:,:) = tcoeff*(sigma(:,:)-sigma_b)
-          tdamp(:,:,k) = tka + cos_lat_4(:,:)*tfactr(:,:)
-        elsewhere
-          tdamp(:,:,k) = tka
-        endwhere
-      endif
+      where (sigma(:,:) <= 1.0 .and. sigma(:,:) > sigma_b)
+        tfactr(:,:) = tcoeff*(sigma(:,:)-sigma_b)
+        tdamp(:,:,k) = tka + cos_lat_4(:,:)*tfactr(:,:)
+      elsewhere
+        tdamp(:,:,k) = tka
+      endwhere
 
       enddo
 
