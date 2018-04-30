dbdd401f8bddb4d5630b064080b564be859d0543
sit23 <stephen.i.thomson@gmail.com>
2018-04-30 11:50:46 +0100

    Adding a longer socrates test for running elsewhere.

---
+++ exp/socrates_test/long_socrates_test.py
@@ -0,0 +1,203 @@
+import os
+
+import numpy as np
+
+from isca import SocratesCodeBase, DiagTable, Experiment, Namelist, GFDL_BASE
+from isca.util import exp_progress
+
+NCORES = 16
+base_dir = os.path.dirname(os.path.realpath(__file__))
+# a CodeBase can be a directory on the computer,
+# useful for iterative development
+cb = SocratesCodeBase.from_directory(GFDL_BASE)
+
+# or it can point to a specific git repo and commit id.
+# This method should ensure future, independent, reproducibility of results.
+# cb = DryCodeBase.from_repo(repo='https://github.com/isca/isca', commit='isca1.1')
+
+# compilation depends on computer specific settings.  The $GFDL_ENV
+# environment variable is used to determine which `$GFDL_BASE/src/extra/env` file
+# is used to load the correct compilers.  The env file is always loaded from
+# $GFDL_BASE and not the checked out git repo.
+
+cb.compile(debug=False)  # compile the source code to working directory $GFDL_WORK/codebase
+
+# create an Experiment object to handle the configuration of model parameters
+# and output diagnostics
+exp = Experiment('soc_test_mk37_long_diurnal', codebase=cb)
+
+exp.inputfiles = [os.path.join(base_dir,'input/co2.nc'), os.path.join(GFDL_BASE,'input/rrtm_input_files/ozone_1990.nc')]
+
+#Tell model how to write diagnostics
+diag = DiagTable()
+diag.add_file('atmos_monthly', 30, 'days', time_units='days')
+
+#Tell model which diagnostics to write
+diag.add_field('dynamics', 'ps', time_avg=True)
+diag.add_field('dynamics', 'bk')
+diag.add_field('dynamics', 'pk')
+diag.add_field('atmosphere', 'precipitation', time_avg=True)
+diag.add_field('mixed_layer', 't_surf', time_avg=True)
+diag.add_field('dynamics', 'sphum', time_avg=True)
+diag.add_field('dynamics', 'ucomp', time_avg=True)
+diag.add_field('dynamics', 'vcomp', time_avg=True)
+diag.add_field('dynamics', 'temp', time_avg=True)
+diag.add_field('dynamics', 'vor', time_avg=True)
+diag.add_field('dynamics', 'div', time_avg=True)
+
+# diag.add_field('socrates', 'soc_spectral_olr', time_avg=True)
+#diag.add_field('socrates', 'soc_olr', time_avg=True)
+# diag.add_field('socrates', 'soc_olr_spectrum_lw', time_avg=True)
+# diag.add_field('socrates', 'soc_surf_spectrum_sw', time_avg=True)
+#diag.add_field('socrates', 'soc_heating_lw', time_avg=True)
+#diag.add_field('socrates', 'soc_heating_sw', time_avg=True)
+#diag.add_field('socrates', 'soc_heating_rate', time_avg=True)
+#diag.add_field('socrates', 'soc_flux_up_lw', time_avg=True)
+#diag.add_field('socrates', 'soc_flux_down_sw', time_avg=True)
+
+
+exp.diag_table = diag
+
+#Empty the run directory ready to run
+exp.clear_rundir()
+
+#Define values for the 'core' namelist
+exp.namelist = namelist = Namelist({
+    'main_nml':{
+     'days'   : 30,
+     'hours'  : 0,
+     'minutes': 0,
+     'seconds': 0,
+     'dt_atmos':720,
+     'current_date' : [1,1,1,0,0,0],
+     'calendar' : 'thirty_day'
+    },
+    'socrates_rad_nml': {
+        'stellar_constant':1370.,
+        'lw_spectral_filename':os.path.join(GFDL_BASE,'src/atmos_param/socrates/src/trunk/data/spectra/ga7/sp_lw_ga7'),
+        'sw_spectral_filename':os.path.join(GFDL_BASE,'src/atmos_param/socrates/src/trunk/data/spectra/ga7/sp_sw_ga7'),
+        'tidally_locked': False,
+        'do_read_ozone': True,
+        'ozone_file_name':'ozone_1990',
+        'ozone_field_name':'ozone_1990',
+        'do_read_co2': True,
+        'dt_rad':4320,
+        'store_intermediate_rad':True,
+    }, 
+    'idealized_moist_phys_nml': {
+        'do_damping': True,
+        'turb':True,
+        'mixed_layer_bc':True,
+        'do_virtual' :False,
+        'do_simple': True,
+        'roughness_mom':3.21e-05,
+        'roughness_heat':3.21e-05,
+        'roughness_moist':3.21e-05,            
+        'two_stream_gray': False,     #Use the grey radiation scheme
+        'do_socrates_radiation': True,
+        'convection_scheme': 'SIMPLE_BETTS_MILLER', #Use simple Betts miller convection            
+    },
+
+    'vert_turb_driver_nml': {
+        'do_mellor_yamada': False,     # default: True
+        'do_diffusivity': True,        # default: False
+        'do_simple': True,             # default: False
+        'constant_gust': 0.0,          # default: 1.0
+        'use_tau': False
+    },
+    
+    'diffusivity_nml': {
+        'do_entrain':False,
+        'do_simple': True,
+    },
+
+    'surface_flux_nml': {
+        'use_virtual_temp': False,
+        'do_simple': True,
+        'old_dtaudv': True    
+    },
+
+    'atmosphere_nml': {
+        'idealized_moist_model': True
+    },
+
+    #Use a large mixed-layer depth, and the Albedo of the CTRL case in Jucker & Gerber, 2017
+    'mixed_layer_nml': {
+        'tconst' : 285.,
+        'prescribe_initial_dist':True,
+        'evaporation':True,  
+        'depth': 2.5,                          #Depth of mixed layer used
+        'albedo_value': 0.38,                  #Albedo value used      
+    },
+
+    'qe_moist_convection_nml': {
+        'rhbm':0.7,
+        'Tmin':160.,
+        'Tmax':350.   
+    },
+    
+    'lscale_cond_nml': {
+        'do_simple':True,
+        'do_evap':True
+    },
+    
+    'sat_vapor_pres_nml': {
+        'do_simple':True
+    },
+    
+    'damping_driver_nml': {
+        'do_rayleigh': True,
+        'trayfric': -0.25,              # neg. value: time in *days*
+        'sponge_pbottom':  5000., #Bottom of the model's sponge down to 50hPa
+        'do_conserve_energy': True,    
+    },
+
+    'two_stream_gray_rad_nml': {
+        'rad_scheme':  'byrne',        #Select radiation scheme to use
+        'atm_abs': 0.2,                      # Add a bit of solar absorption of sw
+        'do_seasonal':  True,          #do_seasonal=false uses the p2 insolation profile from Frierson 2006. do_seasonal=True uses the GFDL astronomy module to calculate seasonally-varying insolation.
+        'equinox_day':  0.75,          #A calendar parameter to get autumn equinox in september, as in the standard earth calendar.
+        'do_read_co2':  True, #Read in CO2 timeseries from input file
+        'co2_file':  'co2', #Tell model name of co2 input file        
+    },
+
+    # FMS Framework configuration
+    'diag_manager_nml': {
+        'mix_snapshot_average_fields': False  # time avg fields are labelled with time in middle of window
+    },
+
+    'fms_nml': {
+        'domains_stack_size': 600000                        # default: 0
+    },
+
+    'fms_io_nml': {
+        'threading_write': 'single',                         # default: multi
+        'fileset_write': 'single',                           # default: multi
+    },
+
+    'spectral_dynamics_nml': {
+        'damping_order': 4,             
+        'water_correction_limit': 200.e2,
+        'reference_sea_level_press':1.0e5,
+        'num_levels':25,      #How many model pressure levels to use
+        'valid_range_t':[100.,800.],
+        'initial_sphum':[2.e-6],
+        'vert_coord_option':'input',#Use the vertical levels from Frierson 2006
+        'surf_res':0.5,
+        'scale_heights' : 11.0,
+        'exponent':7.0,
+        'robert_coeff':0.03
+    },
+    'vert_coordinate_nml': {
+        'bk': [0.000000, 0.0117665, 0.0196679, 0.0315244, 0.0485411, 0.0719344, 0.1027829, 0.1418581, 0.1894648, 0.2453219, 0.3085103, 0.3775033, 0.4502789, 0.5244989, 0.5977253, 0.6676441, 0.7322627, 0.7900587, 0.8400683, 0.8819111, 0.9157609, 0.9422770, 0.9625127, 0.9778177, 0.9897489, 1.0000000],
+        'pk': [0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000, 0.000000],
+       }
+})
+
+#Lets do a run!
+if __name__=="__main__":
+
+    s = 1.0
+    exp.run(1, use_restart=False, num_cores=NCORES, run_idb=False)
+    for i in range(2,121):
+        exp.run(i, num_cores=NCORES)
