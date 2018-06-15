ac424cbedb8796e5dc786f743d470c56f59403ed
sit23 <stephen.i.thomson@gmail.com>
2018-06-15 17:17:20 +0200

    Updated compile script for interpolator so that it compiles on argo.

--- postprocessing/plevel_interpolation/compile_plev_interpolation.sh
+++ postprocessing/plevel_interpolation/compile_plev_interpolation.sh
@@ -5,6 +5,6 @@ cd ./exec
 
 source $GFDL_BASE/src/extra/env/$GFDL_ENV
 
-../bin/mkmf -p plev.x -t ../bin/mkmf.template.ia64 -c "-Duse_netCDF" -a ../src ../src/path_names ../src/shared/mpp/include ../src/shared/include
+../bin/mkmf -p plev.x -t $GFDL_BASE/src/extra/python/isca/templates/mkmf.template.ia64 -c "-Duse_netCDF" -a ../src ../src/path_names ../src/shared/mpp/include ../src/shared/include
 
 make -f Makefile
