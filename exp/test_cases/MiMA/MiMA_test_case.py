009716603c09b22637a04d248be6193a3cefabfa
Penelope Maher <p.maher@exeter.ac.uk>
2018-09-06 16:27:53 +0100

    Swapped to using mcica sw routine and added required initialisation routine. Seems to run. Now swapping to lw mcica.

--- exp/test_cases/MiMA/MiMA_test_case.py
+++ exp/test_cases/MiMA/MiMA_test_case.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from isca import IscaCodeBase, DiagTable, Experiment, Namelist, GFDL_BASE
 
-NCORES = 1
+NCORES = 16
 
 # a CodeBase can be a directory on the computer,
 # useful for iterative development
