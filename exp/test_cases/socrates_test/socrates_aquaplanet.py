46ef7a82ee990ab258710340574b9c6a8d1074dd
Penelope Maher <p.maher@exeter.ac.uk>
2018-09-12 16:42:36 +0100

    Remove debug and account for clouds flag

--- exp/test_cases/socrates_test/socrates_aquaplanet.py
+++ exp/test_cases/socrates_test/socrates_aquaplanet.py
@@ -5,7 +5,7 @@ import numpy as np
 from isca import SocratesCodeBase, DiagTable, Experiment, Namelist, GFDL_BASE
 from isca.util import exp_progress
 
-NCORES = 1
+NCORES = 16
 base_dir = os.path.dirname(os.path.realpath(__file__))
 # a CodeBase can be a directory on the computer,
 # useful for iterative development
@@ -109,7 +109,7 @@ exp.namelist = namelist = Namelist({
         'two_stream_gray': False,     #Use the grey radiation scheme
         'do_socrates_radiation': True,
         'convection_scheme': 'SIMPLE_BETTS_MILLER', #Use simple Betts miller convection            
-        'do_cloud_simple': True,
+        'do_cloud_simple': True
     },
 
     'cloud_simple_nml': {
