b16a01ed0aa796b5a2d37972223934535c213a4f
sit23 <stephen.i.thomson@gmail.com>
2018-12-06 15:48:09 +0000

    Made some changes to socrates cloud test experiment.

--- exp/test_cases/socrates_test/socrates_aquaplanet_with_cloud.py
+++ exp/test_cases/socrates_test/socrates_aquaplanet_with_cloud.py
@@ -81,8 +81,8 @@ diag.add_field('cloud_simple', 'rh_min', time_avg=True)
 diag.add_field('cloud_simple', 'rh_in_cf', time_avg=True)
 
 # additional output options commented out 
-#diag.add_field('socrates', 'soc_flux_lw', time_avg=True)
-#diag.add_field('socrates', 'soc_flux_sw', time_avg=True)
+diag.add_field('socrates', 'soc_flux_lw', time_avg=True)
+diag.add_field('socrates', 'soc_flux_sw', time_avg=True)
 #diag.add_field('socrates', 'soc_co2', time_avg=True)
 #diag.add_field('socrates', 'soc_ozone', time_avg=True) 
 #diag.add_field('socrates', 'soc_coszen', time_avg=True) 
