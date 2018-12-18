32b5d9489966899e399ef25f3c332c363433f780
Marianne Pietschnig <mp586@exeter.ac.uk>
2018-12-18 18:41:15 +0000

    Remove all inputs and scripts that don't belong here for zonsymm eqmax runs

--- exp/mp586/aquaplanet/aquaplanet_fixedSSTs_10mwinds_test.py
+++ exp/mp586/aquaplanet/aquaplanet_fixedSSTs_10mwinds_test.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from isca import IscaCodeBase, DiagTable, Experiment, Namelist, GFDL_BASE
 
-NCORES = 16
+NCORES = 8
 base_dir = os.getcwd()
 # a CodeBase can be a directory on the computer,
 # useful for iterative development
