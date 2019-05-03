6e5e72e2fbe0292b05bfde89ac5762cd2cad5a59
George Lancaster <qv18258@bristol.ac.uk>
2019-05-03 15:11:41 +0100

    compile.py

--- benchmarking/compile.py
+++ benchmarking/compile.py
@@ -2,17 +2,7 @@ import numpy as np
 
 from isca import IscaCodeBase, GreyCodeBase, DiagTable, Experiment, Namelist, GFDL_BASE
 from isca.util import exp_progress
-from util import get_core_list
-import sh
-import os
-import namelist
 
-# from ntfy import notify
 
-NCORES = 16
-CORE_LIST = get_core_list()
-
-# a CodeBase can be a directory on the computer,
-# useful for iterative development
 cb = GreyCodeBase.from_directory(GFDL_BASE)
 cb.compile()  # compile the source code to working directory $GFDL_WORK/codebase
