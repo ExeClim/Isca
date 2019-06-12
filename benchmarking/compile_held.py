8d0a7e112348ca6d31d3784e3fe6ebd19cb47157
George Lancaster <qv18258@bristol.ac.uk>
2019-06-12 11:49:50 +0100

    compile DryCodeBase

---
+++ benchmarking/compile_held.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+from isca import IscaCodeBase, DryCodeBase, DiagTable, Experiment, Namelist, GFDL_BASE
+from isca.util import exp_progress
+
+
+cb = DryCodeBase.from_directory(GFDL_BASE)
+cb.compile()  # compile the source code to working directory $GFDL_WORK/codebase
