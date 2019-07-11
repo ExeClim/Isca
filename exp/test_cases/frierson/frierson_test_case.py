fa78e6608f4a8fe31d4c0c51c6f2dfdda1f04059
George Lancaster <qv18258@bristol.ac.uk>
2019-07-11 14:54:57 +0100

    Revert overwrite_data=True

--- exp/test_cases/frierson/frierson_test_case.py
+++ exp/test_cases/frierson/frierson_test_case.py
@@ -173,6 +173,6 @@ exp.namelist = namelist = Namelist({
 
 #Lets do a run!
 if __name__=="__main__":
-    exp.run(1, use_restart=False, overwrite_data=True, num_cores=NCORES)
+    exp.run(1, use_restart=False, num_cores=NCORES)
     for i in range(2,121):
         exp.run(i, num_cores=NCORES)
