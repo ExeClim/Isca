65e8ebdec39303df118bc62a892c8e36b19b70fd
George Lancaster <qv18258@newblue4.cm.cluster>
2019-07-11 16:11:39 +0100

    preparation for merge with uob_benchamrking

--- benchmarking/compile_scripts/held_suarez_compile.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-from isca import IscaCodeBase, DryCodeBase, DiagTable, Experiment, Namelist, GFDL_BASE
-from isca.util import exp_progress
-
-
-cb = DryCodeBase.from_directory(GFDL_BASE)
-cb.compile(debug=True)  # compile the source code to working directory $GFDL_WORK/codebase
--- postprocessing/mppnccombine_run.sh
+++ postprocessing/mppnccombine_run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # 1. Load the necessary tools into the environment
-#module purge
+if [[ -z "${GFDL_PURGE}" ]] || [ "${GFDL_PURGE}" = true ]; then
+    module purge
+fi
 source $GFDL_BASE/src/extra/env/$GFDL_ENV
 module list
 
