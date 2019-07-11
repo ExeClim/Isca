2078dfd0b1f1911b965c133ec58315cb39f1cf6d
George Lancaster <qv18258@bristol.ac.uk>
2019-07-11 16:17:11 +0100

    Check whether it is safe to purge

--- postprocessing/mppnccombine_run.sh
+++ postprocessing/mppnccombine_run.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-# 1. Load the necessary tools into the environment
+# 1. If safe to purge, then do so
 if [[ -z "${GFDL_PURGE}" ]] || [ "${GFDL_PURGE}" = true ]; then
     module purge
 fi
+# Load the necessary tools into the environment
 source $GFDL_BASE/src/extra/env/$GFDL_ENV
 module list
 
