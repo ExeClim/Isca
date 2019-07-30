acd283a427beeb920bd280e7a9b94c0e45ef7372
sit23 <stephen.i.thomson@gmail.com>
2019-07-30 16:16:44 +0100

    Added equinox day to hs forcing so it can easily do earth calendars.

--- src/atmos_param/hs_forcing/hs_forcing.F90
+++ src/atmos_param/hs_forcing/hs_forcing.F90
@@ -101,6 +101,7 @@ private
    real :: heat_capacity=4.2e6      ! equivalent to a 1m mixed layer water ocean
    real :: ml_depth=1               ! depth for heat capacity calculation
    real :: spinup_time=10800.     ! number of days to spin up heat capacity for - req. multiple of orbital_period
+   real :: equinox_day = 0.
 
 
 !-----------------------------------------------------------------------
@@ -115,7 +116,8 @@ private
                               u_wind_file, v_wind_file, equilibrium_t_option,&
                               equilibrium_t_file, p_trop, alpha, peri_time, smaxis, albedo, &
                               lapse, h_a, tau_s, orbital_period,         &
-                              heat_capacity, ml_depth, spinup_time, stratosphere_t_option
+                              heat_capacity, ml_depth, spinup_time, stratosphere_t_option, & 
+                              equinox_day
 
 !-----------------------------------------------------------------------
 
@@ -828,7 +830,7 @@ real :: theta, mean_anomaly, ecc_anomaly, true_anomaly
     call calc_ecc_anomaly(mean_anomaly, ecc, ecc_anomaly)
     true_anomaly = 2*atan(((1 + ecc)/(1 - ecc))**0.5 * tan(ecc_anomaly/2))
     orb_dist = smaxis * (1 - ecc**2)/(1 + ecc*cos(true_anomaly))
-    theta = 2*pi*current_time/(orbital_period*86400)
+    theta = 2*pi*modulo((current_time/(orbital_period*86400))-equinox_day, 1.0)
     dec = asin(sin(obliq*pi/180)*sin(theta))
 
 end subroutine update_orbit
