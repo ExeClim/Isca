7a552690f76e2eae98e56e0656a5075d95e9c387
sit23 <stephen.i.thomson@gmail.com>
2020-01-31 13:05:37 +0000

    Added option to make mkmf template the same as that used by compile.sh.

--- postprocessing/plevel_interpolation/compile_plev_interpolation.sh
+++ postprocessing/plevel_interpolation/compile_plev_interpolation.sh
@@ -5,6 +5,9 @@ cd ./exec
 
 source $GFDL_BASE/src/extra/env/$GFDL_ENV
 
-../bin/mkmf -p plev.x -t $GFDL_BASE/src/extra/python/isca/templates/mkmf.template.ia64 -c "-Duse_netCDF" -a ../src ../src/path_names ../src/shared/mpp/include ../src/shared/include
+compiler=${GFDL_MKMF_TEMPLATE:-ia64}
+template=mkmf.template.${compiler}
+
+../bin/mkmf -p plev.x -t $GFDL_BASE/src/extra/python/isca/templates/$template -c "-Duse_netCDF" -a ../src ../src/path_names ../src/shared/mpp/include ../src/shared/include
 
 make -f Makefile
