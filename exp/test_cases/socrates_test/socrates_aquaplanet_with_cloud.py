cbdce6d159b31a5a95369a6cfc1ae37e9088f107
Penelope Maher <p.maher@exeter.ac.uk>
2021-01-22 08:56:13 +0000

    Update comments

--- exp/test_cases/socrates_test/socrates_aquaplanet_with_cloud.py
+++ exp/test_cases/socrates_test/socrates_aquaplanet_with_cloud.py
@@ -42,8 +42,8 @@ diag.add_field('dynamics', 'zsurf')
 diag.add_field('atmosphere', 'precipitation', time_avg=True)
 diag.add_field('atmosphere', 'rh', time_avg=True)
 diag.add_field('mixed_layer', 't_surf', time_avg=True)
-diag.add_field('mixed_layer', 'flux_t', time_avg=True) #LH
-diag.add_field('mixed_layer', 'flux_lhe', time_avg=True) #SH
+diag.add_field('mixed_layer', 'flux_t', time_avg=True) #SH
+diag.add_field('mixed_layer', 'flux_lhe', time_avg=True) #LH
 diag.add_field('dynamics', 'sphum', time_avg=True)
 diag.add_field('dynamics', 'ucomp', time_avg=True)
 diag.add_field('dynamics', 'vcomp', time_avg=True)
