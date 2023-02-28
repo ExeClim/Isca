424871f432694cf6852171e9acfac8776aac234f
Daniel Williams <dw569@exeter.ac.uk>
2023-02-28 15:41:01 +0000

    More debugging to run trip tests

--- src/atmos_spectral/driver/solo/idealized_moist_phys.F90
+++ src/atmos_spectral/driver/solo/idealized_moist_phys.F90
@@ -1136,8 +1136,8 @@ if(do_rrtm_radiation) then
    call interp_temp(z_full(:,:,:,current),z_half(:,:,:,current),tg_interp, Time)
    call run_rrtmg(is,js,Time,rad_lat(:,:),rad_lon(:,:),p_full(:,:,:,current),p_half(:,:,:,current),  &
                   albedo,grid_tracers(:,:,:,previous,nsphum),tg_interp,t_surf(:,:),dt_tg(:,:,:),     &
-                  coszen,net_surf_sw_down(:,:),surf_lw_down(:,:), cf_rad(:,:,:), reff_rad(:,:,:),   &
-                  do_cloud_simple )
+                  coszen,net_surf_sw_down(:,:),surf_lw_down(:,:))!, cf_rad(:,:,:), reff_rad(:,:,:),   &
+                  !do_cloud_simple )
 endif
 #endif
 
